6d4c3d6815519d37f24c3125b4c55aa175a31363
SamarthNamdeo <samarthnmd@gmail.com>
2026-05-29 16:17:07 +0530

    Add files via upload

--- data/app.py
+++ data/app.py
@@ -1,35 +1,31 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
 
-
-from dash import Dash, html, dcc
-import plotly.express as px
-import pandas as pd
+from dash import Dash, callback, html, Input, Output, ctx, callback
 
 app = Dash()
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Morsel": ["pink morsel", "gold morsel", "magenta morsel", "periwinkle morsel", "Oranges"],
-    "Amount": [2195,2180, 2395, 4045, 4135],
-    "Direction": ["North", "East", "West", "South" , "North"]
-})
-
-fig = px.bar(df, x="Morsel", y="Sales($)", color="Direction", barmode = "group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+app.layout = html.Div([
+    html.Button('Button 1', id='btn-1'),
+    html.Button('Button 2', id='btn-2'),
+    html.Button('Button 3', id='btn-3'),
+    html.Div(id='container'),
+    html.Div(id='container-no-ctx')
 ])
+
+@callback(
+    Output('container-no-ctx', 'children'),
+    Input('btn-1', 'n_clicks'),
+    Input('btn-2', 'n_clicks'))
+def update(btn1, btn2):
+    return f'button 1: {btn1} & button 2: {btn2}'
+
+
+@callback(Output('container','children'),
+              Input('btn-1', 'n_clicks'),
+              Input('btn-2', 'n_clicks'),
+              Input('btn-3', 'n_clicks'))
+def display(btn1, btn2, btn3):
+    button_clicked = ctx.triggered_id
+    return f'You last clicked button with ID {button_clicked}'
 
 if __name__ == '__main__':
     app.run(debug=True)
